015af92c71297116f90b730a615ddd5190192408
Gennadiy Fedyaev <goodbikeshop45@gmail.com>
2015-07-15 18:01:07 +0500

    первый коммит

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require("gulp"),
+	browserSync = require('browser-sync');
+
+gulp.task('server', function (){
+	browserSync({
+		port: 9000,
+		server: {
+			baseDir: 'src'
+		}
+	});	
+});	
+
+gulp.task('watch', function (){
+	gulp.watch ([
+		'src/*.html',
+		'src/js/**/*.js',
+		'src/css/**/*.css'
+		]).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['server', 'watch']);
+
+
